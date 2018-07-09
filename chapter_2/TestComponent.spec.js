e751b6d4515dac575c4ad57eb819aa8c36234c3c
Dmytro Cisar <dmytro.cisar@gmail.com>
2018-07-09 23:45:41 +0300

    + deps and rename files

--- chapter_2/TestComponent.spec.js
+++ chapter_2/TestComponent.spec.js
@@ -1,3 +1,6 @@
+import { shallow } from 'vue-test-utils';
+import TestComponent from './TestComponent.vue';
+
 test('renders Hello, World!', () => {
     const wrapper = shallow(TestComponent);
     expect(wrapper.text()).toContain('Hello, World!');

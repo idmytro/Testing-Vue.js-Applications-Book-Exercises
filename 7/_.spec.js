1e835b2a914b727c53ea5e5e53437e717c41acd7
Dmytro Cisar <dmytro.cisar@gmail.com>
2018-07-24 23:41:46 +0300

    7

--- 7/_.spec.js
+++ 7/_.spec.js
@@ -1,3 +1,21 @@
 /*
 Write the code to shallow mount a component with a Vuex store installed on a localVue. The store config object will be imported from another file
 */
+
+import Vuex from 'vuex'
+import {shallow, createLocalVue} from 'vue-test-utils'
+import storeConfig from './store-config'
+import TestComponent from './TestComponent.vue'
+
+test('mounts correctly', () => {
+  const localVue = createLocalVue()
+
+  localVue.use(Vuex)
+
+  const store = new Vuex.Store(storeConfig)
+
+  shallow(TestComponent, {
+    localVue,
+    store
+  })
+})
